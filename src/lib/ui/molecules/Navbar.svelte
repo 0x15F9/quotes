e160bd54f83046636eac5f58b4745be2c7ae4622
Isfaaq <isfaaq@bazaks.com>
2022-10-07 23:00:01 +0400

    feat: first components

---
+++ src/lib/ui/molecules/Navbar.svelte
@@ -0,0 +1,3 @@
+<div class="flex border-b-2 border-black justify-center">
+  <span class="border-black px-16 py-10 font-serif text-xl">Quotes</span>
+</div>
---
+++ src/lib/ui/molecules/Quote.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let quote;
+  export let author;
+</script>
+
+<section class="px-20">
+  <h1 class="font-serif text-5xl text-green mb-8">{quote}</h1>
+
+  <div class="flex items-center gap-4">
+    <div class="border-b w-24" />
+    <span class="text-xl">{author}</span>
+  </div>
+</section>
