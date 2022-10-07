af9cb03b1639efcfe46576e089f69f61d5184322
Isfaaq <isfaaq@bazaks.com>
2022-10-07 22:47:16 +0400

    style: add font and colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,15 @@ const config = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
 
   theme: {
+    colors: {
+      beige: "#F4F2E6",
+      green: "#3C541B",
+      black: "1A1D16",
+    },
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+      serif: ["DukeCharming", "serif"],
+    },
     extend: {},
   },
 
